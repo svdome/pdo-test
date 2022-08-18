752dbb9f364591de875132b82776cb165b022770
svdome <svdome@yandex.ru>
2022-08-18 17:32:10 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+
+//print_r(PDO::getAvailableDrivers());
+
+try {
+    $db=new PDO('mysql:host=localhost; dbname=pdo-test', 'root', 'root');
+    echo 'success';
+} catch (PDOException $e) {
+    print_r('Error!: ' .$e->getMessage());
+    die();
+};
+
+
+// прямой запрос однострочный
+/**
+//$exec=$db->exec('insert into categories (name) value (\'Пылесосы\')'); //запись в БД
+
+$res=$db->query('SELECT * from categories');
+    //$row=$res->fetchAll();
+    //     echo '<pre>';
+    //    print_r ($row);
+    //    echo '</pre>';
+    // ИЛИ
+
+while ($row=$res->fetch()) {
+    echo '<pre>';
+    print_r ($row);
+    echo '</pre>';
+};
+*/
+
+// Подготовленный запрос
+///**
+$id=3;
+$stmt=$db->prepare('select name, id from categories'); //подготовленный запрос
+$stmt->execute();
+$category=$stmt->fetchAll(PDO::FETCH_KEY_PAIR);
+
+echo '<pre>';
+print_r($category);
+
+echo '</pre>';
+//*/
+
+
+
+
+/**
+$limit=2;
+$stm=$db->prepare('select*from categories limit ?');
+$stm->bindValue(1, $limit, PDO::PARAM_INT);
+$stm->execute();
+$data=$stm->fetchAll();
+echo '<pre>';
+print_r($data);
+echo '</pre>';
+*/
+
+/**
+$arr=[1,3,6];
+$in = str_repeat('?,', count($arr)-1) . '?';
+$str=$db->prepare("select*from categories where id in ($in)");
+$stm->execute($arr);
+$data=$stm->fetchAll();
+echo '<pre>';
+print_r($data);
+echo '</pre>';
+*/
+
+/**
+$name='новая категория';
+$stm=$db->prepare('insert into categories (name) values (:name)');
+$stm->execute(['name'=>$name]);
+*/
+/**
+$id=3;
+$name= 'Компьютеры';
+$stm=$db->prepare('update categories set name = :name where id =:id');
+$stm->execute(['name'=>$name, 'id'=>$id]);
+*/
+
+/**
+$id=1;
+$stm=$db->prepare('delete from categories where id =:id');
+$stm->execute(['id'=>$id]);
+*/
+
